92c34c6301c1d62edd02d458bffb9d0f9ae54b9b
Vandam Dinh <vandamdinh71@gmail.com>
2023-02-28 17:29:33 +0000

    Updated install script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 if type git > /dev/null; then
     git clone https://github.com/vandamd/dotfiles ~/Downloads/dotfiles
-    chmod +x ~/Downloads/dotfiles
+    chmod +x ~/Downloads/dotfiles/install.sh
     cd ~/Downloads/dotfiles
     ./install.sh
 else
@@ -9,7 +9,7 @@ else
     unzip master.zip
     rm -rf master.zip
     mv dotfiles-main ~/Downloads/dotfiles
-    chmod +x ~/Downloads/dotfiles
+    chmod +x ~/Downloads/dotfiles/install.sh
     cd ~/Downloads/dotfiles
     ./install.sh
 fi
